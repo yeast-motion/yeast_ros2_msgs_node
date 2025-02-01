6b323286795859f1a4a1ee753e6336d70a0890e5
Chris Bonomi <bonomi.c11@gmail.com>
2025-02-01 04:34:15 -0500

    best gen yet fr

--- template/render_jinja.py
+++ template/render_jinja.py
@@ -8,8 +8,10 @@ import re
 
 parser = argparse.ArgumentParser(description='Render script')
 parser.add_argument('--src-dir', action='store', dest='src_dir', default='')
-parser.add_argument('--dst-dir', action='store', dest='dst_dir', default='')
-parser.add_argument('--template', action='store', dest='template', default='')
+parser.add_argument('--header-template', action='store', dest='header_template', default='')
+parser.add_argument('--source-template', action='store', dest='source_template', default='')
+parser.add_argument('--header-dest', action='store', dest='header_dest', default='')
+parser.add_argument('--source-dest', action='store', dest='source_dest', default='')
 args = parser.parse_args()
 
 SCRIPT_DIRECTORY = os.path.dirname(os.path.realpath(__file__))
@@ -18,12 +20,14 @@ ROOT_DIRECTORY = os.path.dirname(SCRIPT_DIRECTORY)
 print(SCRIPT_DIRECTORY)
 print(ROOT_DIRECTORY)
 
-template_file = os.path.realpath(args.template)
+header_template_file = os.path.realpath(args.header_template)
+source_template_file = os.path.realpath(args.source_template)
 
 schema_dir = os.path.realpath(args.src_dir)
 schema_files = glob.glob(f'{schema_dir}/*.json')
 
-header_dir = os.path.realpath(args.dst_dir)
+header_dest = os.path.realpath(args.header_dest)
+source_dest = os.path.realpath(args.source_dest)
 
 header_names = []
 type_members = {}
@@ -56,9 +60,16 @@ for schema_file in schema_files:
     schema_basename = os.path.basename(schema_file)
 
 env = jinja2.Environment(loader=jinja2.FileSystemLoader('/'), trim_blocks=True)
-msg_template = env.get_template(template_file)
+
+msg_template = env.get_template(header_template_file)
 msg_result = msg_template.render(header_names=header_names, type_members=type_members)
-f = open(f'{header_dir}/yeast_msg_helper_gen.hpp', 'w')
+f = open(f'{header_dest}', 'w')
+f.write(msg_result)
+f.close()
+
+msg_template = env.get_template(source_template_file)
+msg_result = msg_template.render(type_members=type_members)
+f = open(f'{source_dest}', 'w')
 f.write(msg_result)
 f.close()
 
